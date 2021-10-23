2f015046628cd6a1b822288d02b9bd3c218c172c
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2021-10-23 20:20:26 +0700

    change to function.

--- ku_lend/models.py
+++ ku_lend/models.py
@@ -4,12 +4,12 @@ from django.db import models
 from django.utils import timezone
 
 
-class Fee(models.Model):
-    """fee"""
-    start_date = models.DateTimeField()
-    last_date = models.DateTimeField()
-    fee = 100
-
-    def count_fee(self):
-        if self.last_date > self.start_date:
-            return (self.last_date - self.start_date) * self.fee
+# class Fee(models.Model):
+#     """fee"""
+#     start_date = models.DateTimeField()
+#     last_date = models.DateTimeField()
+#     fee = 100
+#
+#     def count_fee(self):
+#         if self.last_date > self.start_date:
+#             return (self.last_date - self.start_date) * self.fee
