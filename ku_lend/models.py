611c4523bd367768b7becd125c2fefc8f1daa9f7
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2021-10-23 07:05:45 +0700

    create fee class and count_fee.

--- ku_lend/models.py
+++ ku_lend/models.py
@@ -4,21 +4,12 @@ from django.db import models
 from django.utils import timezone
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Fee(models.Model):
+    """fee"""
+    start_date = models.DateTimeField()
+    last_date = models.DateTimeField()
+    fee = 100
 
-    def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
+    def count_fee(self):
+        if self.last_date > self.start_date:
+            return (self.last_date - self.start_date) * self.fee
