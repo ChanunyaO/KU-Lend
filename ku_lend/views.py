c13e07ec691988e30f3b1a309434fdb300a3a04c
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2021-10-30 16:29:45 +0700

    add bootstrap,add navbar,change some code.

--- ku_lend/views.py
+++ ku_lend/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
-
 from .models import History, Item
 
 
 def index(request):
+    """Query Data from model"""
+    all_item = Item.objects.all()
     latest_item_list = Item.objects.order_by('-item_name')[:5]
     context = {'latest_item_list': latest_item_list}
-    return render(request, 'ku_lend/index.html', context)
+    return render(request, 'ku_lend/index.html', {'items':all_item})
 
 def borrow_form(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
