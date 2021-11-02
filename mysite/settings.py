3586a926db931bb4dda501d407198780bfa8d8e3
Sujinunt Puangmalai <sujinunt555@hotmail.com>
2021-11-02 19:05:51 +0700

    Fix admin page

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,8 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -129,6 +130,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -154,7 +160,7 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
-django_heroku.settings(locals(), staticfiles=False, allowed_hosts=False)
+django_heroku.settings(locals())
 if "DYNO" in os.environ:
     STATIC_ROOT = 'static'
     ALLOWED_HOSTS = ['kulend.herokuapp.com']
