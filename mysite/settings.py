b575f5de6b67998ffe508e91772a307b6dc04224
Sujinunt Puangmalai <sujinunt555@hotmail.com>
2021-11-02 17:34:16 +0700

    Move static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import dj_database_url
 import os
-from decouple import config
-from pathlib import Path
+#from decouple import config
+#from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
