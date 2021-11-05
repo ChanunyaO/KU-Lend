33ff91570fd7b5d228f96727c483c8678bc1c3bb
Wha Chanunya <whachanunya@gmail.com>
2021-11-05 23:01:30 +0700

    add tested sending function

--- ku_lend/function/reminder.py
+++ ku_lend/function/reminder.py
@@ -4,14 +4,26 @@ import datetime
 from ku_lend.models import *
 from django.core.mail import send_mail
 
+from mysite.settings import EMAIL_HOST_USER
+
 
 def send_reminder():
     """Send reminder before the return date."""
     now = timezone.now()
     if now + datetime.timedelta(days=2) == History.return_date:
         send_mail('Reminder',
-                  f'Please return {Item.item_name} on {History.return_date}',
-                  None,
-                  [History.borrower_email]
+                  f'Please return the item within the returning date',
+                  EMAIL_HOST_USER,
+                  [borrower]
                   )
+    return None
+
+
+# tested sending mail
+def try_send_mail():
+    send_mail('Reminder',
+    f'Please return the item within the returning date',
+    EMAIL_HOST_USER,
+    ['remegac219@ecofreon.com']
+    )
     return None
