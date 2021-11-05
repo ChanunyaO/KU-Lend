b01c7acd396095d7cbd1d32506c68c4aa5deabc7
Wha Chanunya <whachanunya@gmail.com>
2021-11-05 21:23:16 +0700

    change to equal sign

--- ku_lend/function/reminder.py
+++ ku_lend/function/reminder.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 def send_reminder():
     """Send reminder before the return date."""
     now = timezone.now()
-    if now + datetime.timedelta(days=2) >= History.return_date:
+    if now + datetime.timedelta(days=2) == History.return_date:
         send_mail('Reminder',
                   f'Please return {Item.item_name} on {History.return_date}',
                   None,
