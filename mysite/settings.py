d18acbadb3377f2502e3fe4c63536edad6aaf78a
Sujinunt Puangmalai <sujinunt555@hotmail.com>
2021-11-06 15:52:01 +0700

    Fix migrate

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+#from decouple import config
 import dj_database_url
 import os
 import django_heroku
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+#SECRET_KEY = config('SECRET_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
