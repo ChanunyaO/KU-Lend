c39c99d6f8d0185804b9c22209e4c4fad5663cf6
Wha Chanunya <whachanunya@gmail.com>
2021-11-11 11:16:03 +0700

    fix the function and write billing testcase

--- ku_lend/tests/test_emails.py
+++ ku_lend/tests/test_emails.py
@@ -1,5 +1,5 @@
 from ku_lend.models import Item, History
-from ku_lend.function import reminder, confirm_mail
+from ku_lend.function import reminder, confirm_mail, bill
 from django.test import TestCase
 from django.contrib.auth.models import User
 from datetime import timedelta
@@ -121,3 +121,70 @@ class ConfirmTests(TestCase):
         confirm = confirm_mail.send_confirm(self.borrower1.borrower, self.item, self.borrower1.borrower_email)
         self.assertEqual(confirm, self.borrower1.borrower_email)
 
+
+class BillingTests(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.item = Item.objects.create(
+                                    item_name = "Ipad",
+                                    pickup_place = "Computer Department",
+                                    owner = "Aj. Annan",
+                                    note = "-",
+                                    pub_date = timezone.now() - timedelta(days=20),
+                                    amount_items = 10,
+                                    rate_fee = 20,
+                                    max_item_each_user = 1,
+                                    max_day_each_user = 10                          
+                                    )
+        self.item.save()
+
+        self.borrower1 = History.objects.create(
+                                        item = self.item,
+                                        borrow_date = timezone.now(),
+                                        return_date = timezone.now() + timedelta(days=1),
+                                        borrower = "Borrower1",
+                                        borrower_email = "borrower1@test.com",
+                                        borrower_fee = 0,
+                                        borrower_paid_status = "Not paid",
+                                        borrow_amount = 1
+                                        )
+        self.borrower1.save()
+
+        self.borrower2 = History.objects.create(
+                                        item = self.item,
+                                        borrow_date = timezone.now(),
+                                        return_date = timezone.now(),
+                                        borrower = "Borrower2",
+                                        borrower_email = "borrower2@test.com",
+                                        borrower_fee = 0,
+                                        borrower_paid_status = "Not paid",
+                                        borrow_amount = 1
+                                        )
+        self.borrower2.save()
+
+        self.borrower3 = History.objects.create(
+                                        item = self.item,
+                                        borrow_date = timezone.now() - timedelta(days=4),
+                                        return_date = timezone.now() - timedelta(days=10),
+                                        borrower = "Borrower3",
+                                        borrower_email = "borrower3@test.com",
+                                        borrower_fee = 0,
+                                        borrower_paid_status = "Not paid",
+                                        borrow_amount = 1
+                                        )
+        self.borrower2.save()
+
+    def test_before_return_date(self):
+        """The server will not send email before the return date."""
+        billing_func = bill.send_bill()
+        self.assertNotIn(self.borrower1.borrower_email, billing_func)
+
+    def test_now(self):
+        """The server will not send email on the return date."""
+        billing_func = bill.send_bill()
+        self.assertNotIn(self.borrower2.borrower_email, billing_func)
+        
+    def test_after(self):
+        """The server will send email after the return date."""
+        billing_func = bill.send_bill()
+        self.assertIn(self.borrower3.borrower_email, billing_func)
