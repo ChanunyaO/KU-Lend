4a7ccda7ac3f05a1e3c17f8da8729e6ee1983bd1
Wha Chanunya <whachanunya@gmail.com>
2021-11-11 10:58:19 +0700

    edit reminder and reminder's testcase

--- ku_lend/tests/test_emails.py
+++ ku_lend/tests/test_emails.py
@@ -2,7 +2,8 @@ from ku_lend.models import Item, History
 from ku_lend.function import reminder, confirm_mail
 from django.test import TestCase
 from django.contrib.auth.models import User
-from datetime import datetime, timedelta
+from datetime import timedelta
+from django.utils import timezone
 
 
 class ReminderTests(TestCase):
@@ -13,7 +14,7 @@ class ReminderTests(TestCase):
                                     pickup_place = "Computer Department",
                                     owner = "Aj. Annan",
                                     note = "-",
-                                    pub_date = datetime.today() - timedelta(days=10),
+                                    pub_date = timezone.now() - timedelta(days=10),
                                     amount_items = 10,
                                     rate_fee = 20,
                                     max_item_each_user = 1,
@@ -23,8 +24,8 @@ class ReminderTests(TestCase):
 
         self.borrower1 = History.objects.create(
                                         item = self.item,
-                                        borrow_date = datetime.today(),
-                                        return_date = datetime.today() + timedelta(days=10),
+                                        borrow_date = timezone.now(),
+                                        return_date = timezone.now() + timedelta(days=10),
                                         borrower = "Borrower1",
                                         borrower_email = "borrower1@test.com",
                                         borrower_fee = 0,
@@ -35,17 +36,37 @@ class ReminderTests(TestCase):
 
         self.borrower2 = History.objects.create(
                                         item = self.item,
-                                        borrow_date = datetime.today(),
-                                        return_date = datetime.today() + timedelta(days=2),
+                                        borrow_date = timezone.now(),
+                                        return_date = timezone.now() + timedelta(days=2),
                                         borrower = "Borrower2",
                                         borrower_email = "borrower2@test.com",
                                         borrower_fee = 0,
                                         borrower_paid_status = "Not paid",
                                         borrow_amount = 1
                                         )
+        self.borrower2.save()
+
+        self.borrower3 = History.objects.create(
+                                        item = self.item,
+                                        borrow_date = timezone.now() - timedelta(days=4),
+                                        return_date = timezone.now() - timedelta(days=1),
+                                        borrower = "Borrower3",
+                                        borrower_email = "borrower3@test.com",
+                                        borrower_fee = 0,
+                                        borrower_paid_status = "Not paid",
+                                        borrow_amount = 1
+                                        )
+        self.borrower2.save()
 
     def test_before_reminder(self):
         """The server will not send email before the reminder date."""
         reminder_func = reminder.send_reminder()
         self.assertNotIn(self.borrower1.borrower_email, reminder_func)
+
+    def test_2_days_before(self):
+        reminder_func = reminder.send_reminder()
+        self.assertIn(self.borrower2.borrower_email, reminder_func)
         
+    def test_after(self):
+        reminder_func = reminder.send_reminder()
+        self.assertNotIn(self.borrower3.borrower_email, reminder_func)
