c98b9432f4fc2edae3fa0761e360892d98a8241d
Wha Chanunya <whachanunya@gmail.com>
2021-11-21 13:49:35 +0700

    comment test

--- ku_lend/tests/test_e2e_staff.py
+++ ku_lend/tests/test_e2e_staff.py
@@ -1,24 +1,24 @@
-# """Testing using selenium"""
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from django.test import LiveServerTestCase
-# import os
+"""Testing using selenium"""
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
+import os
 
 
-# class SeleniumStaffTests(LiveServerTestCase):
-#     """Test the staff button and admin page."""
+class SeleniumStaffTests(LiveServerTestCase):
+    """Test the staff button and admin page."""
 
-#     def setUp(self):
+    def setUp(self):
 #         """Set up for testing."""
 #         self.browser = webdriver.Chrome('/Users/whachanunya/Desktop/KU-Lend/chromedriver')
 #         url = "http://127.0.0.1:8000/"
 #         self.browser.get(url)
 
-#     def tearDown(self):
+    def tearDown(self):
 #         self.browser.quit()
 
-#     def test_staff_link(self):
-#         """Test if the staff button link to correct link."""
+    def test_staff_link(self):
+        """Test if the staff button link to correct link."""
 #         url_list = []
 #         self.url = self.browser.find_elements_by_tag_name("a")
 #         for url in self.url:
@@ -26,8 +26,8 @@
 #             url_list.append(page_url)
 #         self.assertIn("http://127.0.0.1:8000/admin/", url_list)
 
-#     def test_get_admin_site(self):
-#         """Test can get into admin site which check by the user name."""
+    def test_get_admin_site(self):
+        """Test can get into admin site which check by the user name."""
 #         self.browser.find_elements_by_tag_name("a")[1].click()
 #         self.browser.find_element_by_id("id_username").send_keys("admin1")
 #         self.browser.find_element_by_id("id_password").send_keys("123")
@@ -35,8 +35,8 @@
 #         self.user_name = self.browser.find_element_by_id("user-tools").text
 #         self.assertIn("ADMIN1", self.user_name)
 
-#     def test_can_click_add_item_page(self):
-#         """Test can get into the add item web page."""
+    def test_can_click_add_item_page(self):
+        """Test can get into the add item web page."""
 #         self.browser.find_elements_by_tag_name("a")[1].click()
 #         self.browser.find_element_by_id("id_username").send_keys("admin1")
 #         self.browser.find_element_by_id("id_password").send_keys("123")
@@ -45,8 +45,8 @@
 #         self.page_url = self.page[4].get_attribute('href')
 #         self.assertEqual(self.page_url, "http://127.0.0.1:8000/admin/ku_lend/item/add/")
     
-#     def test_add_item(self):
-#         """Test if add item success."""
+    def test_add_item(self):
+        """Test if add item success."""
 #         self.browser.find_element_by_id("id_username").send_keys("admin1")
 #         self.browser.find_element_by_id("id_password").send_keys("123")
 #         self.browser.find_element_by_id("id_password").send_keys(Keys.RETURN)
