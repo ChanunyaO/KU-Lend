04410de13c70b6474450fbd27eaa975d8f989af8
Sujinunt Puangmalai <sujinunt555@hotmail.com>
2021-11-29 19:06:37 +0700

    Add Cancel Button

--- ku_lend/urls.py
+++ ku_lend/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('<int:item_id>/', views.borrow_form, name='borrow_form'),
     path('<int:item_id>/results/', views.results, name='results'),
     path('<int:item_id>/confirm/', views.confirm, name='confirm'),
+    path('<int:id>/<str:item_name>/<int:amount>/cancel/', views.cancel, name='cancel'),
     path('profile/', profile.profile),
 
 ]
